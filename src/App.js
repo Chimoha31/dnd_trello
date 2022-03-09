7c5d255ff80d9332cea6e96846b9b7aa2da9e9a2
Chimoha31 <mokochii1108@gmail.com>
2022-03-08 19:29:40 -0800

    Be carefull the position of comment

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 function App() {
   return (
-    <div>
-      
+    <div className="dragDropArea">
+      <DragDropContext>
+        <Droppable droppableId="droppable">
+          {/* droppableの直下は関数でなければいけない。 */}
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}> 
+              {/* innerRef=dragしている要素以外の要素の動きを制御する役割 */}
+              <Draggable draggableId="item0" index={0}>
+                {/* draggableの直下は関数でなければいけない。 */}
+                {(provided) => (
+                  <div
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    item0
+                  </div>
+                )}
+              </Draggable>
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 }

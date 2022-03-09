ea8e019a333f69387de9b5cc4486207db381592d
Chimoha31 <mokochii1108@gmail.com>
2022-03-08 20:50:39 -0800

    practice dnd

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
+import { useState } from "react";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import "./App.css";
 
 function App() {
+  const [items] = useState([
+    { id: 0, text: "item0" },
+    { id: 1, text: "item1" },
+    { id: 2, text: "item2" },
+  ]);
+
+  const onDragEnd = (result) => {
+    console.log(result.source.index);
+    console.log(result.destination.index);
+    if (!result.destination) {
+      return;
+    }
+    const remove = items.splice(result.source.index, 1);
+    console.log(remove);
+    const add = items.splice(result.destination.index, 0, remove[0]);
+    return add;
+  };
+
   return (
     <div className="dragDropArea">
-      <DragDropContext>
+      <DragDropContext onDragEnd={onDragEnd}>
         <Droppable droppableId="droppable">
           {/* droppableの直下は関数でなければいけない。 */}
           {(provided) => (
-            <div {...provided.droppableProps} ref={provided.innerRef}> 
+            <div {...provided.droppableProps} ref={provided.innerRef}>
               {/* innerRef=dragしている要素以外の要素の動きを制御する役割 */}
-              <Draggable draggableId="item0" index={0}>
-                {/* draggableの直下は関数でなければいけない。 */}
-                {(provided) => (
-                  <div
-                    ref={provided.innerRef}
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
-                  >
-                    item0
-                  </div>
-                )}
-              </Draggable>
+
+              {items.map((item, index) => (
+                // draggableIdはstring型にする必要があるので、draggableId={item.id}ではエラーになる。ここではdraggableId={item.text}にしておく。
+                <Draggable draggableId={item.text} index={index} key={item.id}>
+                  {/* draggableの直下は関数でなければいけない。 */}
+                  {(provided) => (
+                    <div
+                      className="item"
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                    >
+                      {item.text}
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
             </div>
           )}
         </Droppable>

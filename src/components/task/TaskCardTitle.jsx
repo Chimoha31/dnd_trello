dc5c8ef4a3300e31addc912af557230d83f62269
Chimoha31 <mokochii1108@gmail.com>
2022-03-09 12:21:13 -0800

    Styling for taskcardtitle

--- src/components/task/TaskCardTitle.jsx
+++ src/components/task/TaskCardTitle.jsx
@@ -10,18 +10,30 @@ function TaskCardTitle() {
 
   const handleChange = (e) => {
     setInputCardTitle(e.target.value);
-  }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setIsClick(false);
-  }
+  };
+
+  const handleBlur = () => {
+    setIsClick(false);
+  };
 
   return (
-    <div onClick={handleClick}>
+    <div onClick={handleClick} className="taskCardTitleInputArea">
       {isClick ? (
         <form onSubmit={handleSubmit}>
-          <input type="text" onChange={handleChange} />
+          <input
+            className="taskCardTitleInput"
+            autoFocus
+            type="text"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={inputCardTitle}
+            maxLength="10"
+          />
         </form>
       ) : (
         <h3>{inputCardTitle}</h3>

9b2a6930a64cee81927d0b3dc052fd65dd126965
Chimoha31 <mokochii1108@gmail.com>
2022-03-08 22:44:14 -0800

    Header

--- src/App.js
+++ src/App.js
@@ -1,58 +1,12 @@
-import { useState } from "react";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
-import "./App.css";
+import React from 'react';
+import Header from './components/header/Header';
 
 function App() {
-  const [items] = useState([
-    { id: 0, text: "item0" },
-    { id: 1, text: "item1" },
-    { id: 2, text: "item2" },
-  ]);
-
-  const onDragEnd = (result) => {
-    console.log(result.source.index);
-    console.log(result.destination.index);
-    if (!result.destination) {
-      return;
-    }
-    const remove = items.splice(result.source.index, 1);
-    console.log(remove);
-    const add = items.splice(result.destination.index, 0, remove[0]);
-    return add;
-  };
-
   return (
-    <div className="dragDropArea">
-      <DragDropContext onDragEnd={onDragEnd}>
-        <Droppable droppableId="droppable">
-          {/* droppableの直下は関数でなければいけない。 */}
-          {(provided) => (
-            <div {...provided.droppableProps} ref={provided.innerRef}>
-              {/* innerRef=dragしている要素以外の要素の動きを制御する役割 */}
-
-              {items.map((item, index) => (
-                // draggableIdはstring型にする必要があるので、draggableId={item.id}ではエラーになる。ここではdraggableId={item.text}にしておく。
-                <Draggable draggableId={item.text} index={index} key={item.id}>
-                  {/* draggableの直下は関数でなければいけない。 */}
-                  {(provided) => (
-                    <div
-                      className="item"
-                      ref={provided.innerRef}
-                      {...provided.draggableProps}
-                      {...provided.dragHandleProps}
-                    >
-                      {item.text}
-                    </div>
-                  )}
-                </Draggable>
-              ))}
-              {provided.placeholder}
-            </div>
-          )}
-        </Droppable>
-      </DragDropContext>
+    <div>
+      <Header />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/App2.js
@@ -0,0 +1,58 @@
+// import { useState } from "react";
+// import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+// import "./App2.css";
+
+// function App() {
+//   const [items] = useState([
+//     { id: 0, text: "item0" },
+//     { id: 1, text: "item1" },
+//     { id: 2, text: "item2" },
+//   ]);
+
+//   const onDragEnd = (result) => {
+//     console.log(result.source.index);
+//     console.log(result.destination.index);
+//     if (!result.destination) {
+//       return;
+//     }
+//     const remove = items.splice(result.source.index, 1);
+//     console.log(remove);
+//     const add = items.splice(result.destination.index, 0, remove[0]);
+//     return add;
+//   };
+
+//   return (
+//     <div className="dragDropArea">
+//       <DragDropContext onDragEnd={onDragEnd}>
+//         <Droppable droppableId="droppable">
+//           {/* droppableの直下は関数でなければいけない。 */}
+//           {(provided) => (
+//             <div {...provided.droppableProps} ref={provided.innerRef}>
+//               {/* innerRef=dragしている要素以外の要素の動きを制御する役割 */}
+
+//               {items.map((item, index) => (
+//                 // draggableIdはstring型にする必要があるので、draggableId={item.id}ではエラーになる。ここではdraggableId={item.text}にしておく。
+//                 <Draggable draggableId={item.text} index={index} key={item.id}>
+//                   {/* draggableの直下は関数でなければいけない。 */}
+//                   {(provided) => (
+//                     <div
+//                       className="item"
+//                       ref={provided.innerRef}
+//                       {...provided.draggableProps}
+//                       {...provided.dragHandleProps}
+//                     >
+//                       {item.text}
+//                     </div>
+//                   )}
+//                 </Draggable>
+//               ))}
+//               {provided.placeholder}
+//             </div>
+//           )}
+//         </Droppable>
+//       </DragDropContext>
+//     </div>
+//   );
+// }
+
+// export default App;
